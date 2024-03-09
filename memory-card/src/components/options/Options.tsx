c36bd7a5289fcd1cd09a18f5f08d57832ca9cb31
Емил Стойчев <estallinone@gmail.com>
2024-03-09 11:28:17 +0200

    add options (rules) page and design
    for now

--- memory-card/src/components/options/Options.tsx
+++ memory-card/src/components/options/Options.tsx
@@ -1,15 +1,32 @@
-import * as styles from './style.module.css'
+import * as styles from "./style.module.css";
 import { authActions } from "../../redux/auth-slice";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
-export default function Options({setScreen}) {
-    let dispatch = useDispatch();
+export default function Options({ setScreen }) {
+  let dispatch = useDispatch();
 
-    return (
-        <>
-            <h2>Options page</h2>
+  return (
+    <div className={styles.container}>
+      <h2>Instructions to play memory</h2>
 
-            <button className='primaryBtn' onClick={() => dispatch(authActions.setNewScreen('home'))} >BACK</button>
-        </>
-    )
+      <p>
+        Test your memory with this memory game. First select the difficulty
+        level. The higher the number, the more cards are in the memo game. On
+        the game board, there are always two identical images. Start the game by
+        flipping a card. Then try to find another card that has the same image
+        as the first. If you can't find a pair, the flipped cards will be
+        flipped back with the face down. Try to remember these images as it
+        becomes easier to find pairs the longer you play. When you find a pair
+        they are removed from the board and when you find all the pairs in this
+        memory, you have completed the level.
+      </p>
+
+      <button
+        className="primaryBtn"
+        onClick={() => dispatch(authActions.setNewScreen("home"))}
+      >
+        BACK
+      </button>
+    </div>
+  );
 }
